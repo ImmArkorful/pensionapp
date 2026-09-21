51a2589fb15702b964f8059fe209b9d7461a319e
Vanessa Efakor Akosua Amedi <veamedi@stud.ntnu.no>
2026-09-21 20:13:14 +0200

    Add ScrollToTop component and register all new routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,27 @@ import { RegisterPage } from './pages/RegisterPage'
 import { DashboardPage } from './pages/DashboardPage'
 import { StatementsPage } from './pages/StatementsPage'
 import { SettingsPage } from './pages/SettingsPage'
+import { AboutPage } from './pages/AboutPage'
+import { CareersPage } from './pages/CareersPage'
+import { ProductsPage } from './pages/ProductsPage'
+import { NewsletterPage } from './pages/NewsletterPage'
 import { ProtectedRoute } from './components/layout/ProtectedRoute'
+import { ScrollToTop } from './components/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/faq" element={<FAQPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/careers" element={<CareersPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/newsletter" element={<NewsletterPage />} />
+
         <Route
           path="/dashboard"
           element={
